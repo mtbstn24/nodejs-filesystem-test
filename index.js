04b17802127d88b3af4ba74a00ead6843557aa70
mtbstn24 <thushibstn24@gmail.com>
2023-03-18 00:50:52 +0530

    get average write duration

--- index.js
+++ index.js
@@ -6,34 +6,56 @@ const app = express();
 const fileDir = '../file/';
 const fileSize = 1024;
 const maxFileSize = 1024 * 1024 * 10; //10MB
-const durations = [];
-
-app.use((req,res,next)=>{
-    console.log('Time: ',Date.now());
-    next();
-});
+var durations = [];
 
 const filePath = path.join(fileDir, `file-${fileSize}`)
 const testData = Buffer.alloc(fileSize);
 
-const writeStart = process.hrtime.bigint();
-fs.writeFileSync(filePath,testData);
-const writeEnd = process.hrtime.bigint();
-const writeDuration = (writeEnd - writeStart);
-
-const durationStr = writeDuration.toString();
-const duration = parseInt(durationStr,10)/1000000;
-
-durations.push({
-    size: fileSize,
-    write: writeDuration,
-    duration: duration
+app.use((req,res,next)=>{
+    console.log('\nTime: ', Date.now());
+    next();
 });
 
-console.log(durations);
+function writeProcess(filesize) {
+    durations = [];
+
+    var writeStart = process.hrtime.bigint();
+    fs.writeFileSync(filePath,testData);
+    var writeEnd = process.hrtime.bigint();
+    var writeDuration = (writeEnd - writeStart);
+    var durationStr = writeDuration.toString();
+    var duration = parseInt(durationStr,10)/1000000;
+    
+    var sum=0;
+    
+    for (let index = 0; index < 10; index++) {
+        writeStart = process.hrtime.bigint();
+        fs.writeFileSync(filePath,testData);
+        writeEnd = process.hrtime.bigint();
+        writeDuration = (writeEnd - writeStart);
+        durationStr = writeDuration.toString();
+        duration = parseInt(durationStr,10)/1000000;
+    
+        durations.push({
+            size: fileSize,
+            write: writeDuration,
+            duration: duration
+        });
+    
+        sum = sum + duration;
+    }
+    
+    duration = sum/10;
+    
+    console.log(durations);
+    
+    console.log(`FileSize: ${fileSize}, AvgDuration: ${duration}`);      
+}
 
 app.get('/', (req,res) => {
-    res.send('Connection successful');
+    //res.send('Connection successful');
+    writeProcess();
+    res.send(`fileSize: ${durations[0].size}, duration: ${durations[0].duration}`);
 });
 
 app.listen(3000, ()=> console.log('App listening in port 3000.'));
